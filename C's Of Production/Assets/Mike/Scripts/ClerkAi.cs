cac7340027879a7b6f9662adee0339fdf189f907
Mike <legoguy217@gmail.com>
2018-12-08 00:27:08 -0500

    First push
    Ai patrol
    basic player controller
    'clerk' ai with a dummy shop

---
+++ C's Of Production/Assets/Mike/Scripts/ClerkAi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ClerkAi : MonoBehaviour {
+
+
+    public GameObject captureArea;
+    Rigidbody baseRigidbody;
+    Quaternion rot;
+
+	void Start ()
+    {
+        baseRigidbody = GetComponent<Rigidbody>();
+        rot = transform.rotation;
+	}
+
+    public void ApplyDamage()
+    {
+        baseRigidbody.useGravity = true;
+        baseRigidbody.isKinematic = false;
+        captureArea.SetActive(true);
+    }
+
+    public void ResetClerk(bool captured = false)
+    {
+        captureArea.SetActive(false);
+        baseRigidbody.useGravity = false;
+        baseRigidbody.isKinematic = true;
+        transform.rotation = rot;
+
+        if(captured)
+        {
+            //Change color
+        }
+    }
+}
---
+++ C's Of Production/Assets/Mike/Scripts/Gun.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public int ammo = 50;
+    RaycastHit hit;
+    bool mouselocked;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        mouselocked = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000))
+            {
+
+                if(hit.transform.tag.Equals("Enemy",System.StringComparison.Ordinal))
+                {
+                    hit.transform.SendMessage("ApplyDamage",SendMessageOptions.DontRequireReceiver);
+                    Debug.Log("Enemy Hit!");
+                }
+
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red, 5f);
+                ammo--;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(mouselocked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+}
