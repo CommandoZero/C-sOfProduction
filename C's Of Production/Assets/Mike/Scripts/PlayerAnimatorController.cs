124fc879484b8a02bb9563cd2396d10277e2579b
Mike <legoguy217@gmail.com>
2018-12-08 16:17:41 -0500

    Push

---
+++ C's Of Production/Assets/Mike/Scripts/PlayerAnimatorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour {
+
+    
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            anim.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0f);
+        }
+    }
+}
