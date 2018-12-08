f9dc4d250fd290e881d3f48e20cf7fc71a201f63
Mike <legoguy217@gmail.com>
2018-12-08 10:41:36 -0500

    Update before merge

---
+++ C's Of Production/Assets/Mike/Scripts/LoadingBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour {
+
+    public static LoadingBar instance;
+    public Scrollbar loadingBar;
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public LoadingBar GetInstance()
+    {
+        return instance;
+    }
+
+    //This function expects a value between 1 to 100 to represent percentage
+    public void UpdateLoadingBar(int value)
+    {
+        loadingBar.size = value / 100;
+    }
+
+}
